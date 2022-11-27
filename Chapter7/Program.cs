da05c472210f37893fd62737c35f18c5640daee2
Einar Ingebrigtsen <einari@me.com>
2022-11-27 09:48:15 +0100

    Basic LINQ samples

---
+++ Chapter7/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] items = { 1, 2, 3, 42, 84, 37, 23 };
+
+var filtered = new List<int>();
+foreach (var item in items)
+{
+    if( item >= 37 )
+    {
+        filtered.Add(item);
+    }
+}
+Console.WriteLine(string.Join(',', filtered));
+
+
+var selectedItems =
+    from i in items
+    where i >= 37
+    select i;
+
+Console.WriteLine(string.Join(',', selectedItems));
+
+var selectedItemsExpressions = items
+    .Where(i => i >= 37)
+    .Select(i => i);
+
+Console.WriteLine(string.Join(',', selectedItemsExpressions));
