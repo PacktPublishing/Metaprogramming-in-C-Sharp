0a20c3931c7786d19c6528d0e398984226f929a3
Einar Ingebrigtsen <einari@me.com>
2022-12-17 09:41:14 +0100

    Minor touchups

---
+++ Chapter8/MyType.cs
@@ -0,0 +1,6 @@
+namespace Chapter8;
+
+public class MyType
+{
+    public string StringProperty { get; set; } = String.Empty;
+}
--- Chapter8/Program.cs
+++ Chapter8/Program.cs
@@ -2,6 +2,21 @@
 using System.Text.Json;
 using Chapter8;
 
+// Simple 
+var parameter = Expression.Parameter(typeof(MyType));
+var property = typeof(MyType).GetProperty(nameof(MyType.StringProperty))!;
+var propertyExpression = Expression.Property(parameter,property);
+var assignExpression = Expression.Assign(
+    propertyExpression,
+    Expression.Constant("Hello world"));
+
+var lambdaExpression = Expression.Lambda<Action<MyType>>(assignExpression, parameter);
+var expressionAction = lambdaExpression.Compile();
+var instance = new MyType();
+expressionAction(instance);
+Console.WriteLine(instance.StringProperty);
+
+// Advanced
 var query = File.ReadAllText("query.json");
 var queryDocument = JsonDocument.Parse(query);
 var expression = QueryParser.Parse(queryDocument);
