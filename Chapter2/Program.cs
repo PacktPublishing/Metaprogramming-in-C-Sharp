655d934f25984d79881bdfb5c404871c6f70cc49
Einar Ingebrigtsen <einari@me.com>
2022-12-29 14:12:32 +0100

    Adding sample from book

--- Chapter2/Program.cs
+++ Chapter2/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Chapter2;
+
 Console.WriteLine("Hello, World!");
+
+
+var person = new Person
+{
+    FirstName = "Jane",
+    LastName = "Doe",
+    SocialSecurityNumber = "12345abcd"
+};
+Console.WriteLine(Serializer.SerializeToJson(person));
