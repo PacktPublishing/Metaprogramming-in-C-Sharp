98d0065ccf834c5abfa89a097299329298b4b15b
Einar Ingebrigtsen <einari@me.com>
2023-04-02 11:11:27 +0200

    Loging example done

--- Chapter14/Program.cs
+++ Chapter14/Program.cs
@@ -5,28 +5,30 @@ using Castle.Windsor.Installer;
 using Chapter14;
 using Microsoft.Extensions.Logging;
 
+
+var container = new WindsorContainer();
+container.Install(FromAssembly.InThisApplication(Assembly.GetEntryAssembly()));
+
 var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+container.Register(Component.For<ILoggerFactory>().Instance(loggerFactory));
+
 var createLoggerMethod = typeof(LoggerFactoryExtensions)
     .GetMethods(BindingFlags.Public | BindingFlags.Static)
     .First(_ => _.Name == nameof(LoggerFactory.CreateLogger) && _.IsGenericMethod);
-var logger = loggerFactory.CreateLogger("Root");
 
-var container = new WindsorContainer();
-container.Register(Component.For<ILoggerFactory>().Instance(loggerFactory));
 container.Register(Component.For<ILogger>().UsingFactoryMethod((kernel, context) =>
 {
     var loggerFactory = kernel.Resolve<ILoggerFactory>();
-    return loggerFactory.CreateLogger(context.RequestedType);
-}));
+    return loggerFactory.CreateLogger(context.Handler.ComponentModel.Implementation);
+}).LifestyleTransient());
 container.Register(Component.For(typeof(ILogger<>)).UsingFactoryMethod((kernel, context) =>
 {
     var loggerFactory = kernel.Resolve<ILoggerFactory>();
     var logger = createLoggerMethod.MakeGenericMethod(context.RequestedType.GenericTypeArguments[0]).Invoke(null, new[] { loggerFactory });
     return logger;
 }));
-container.Install(FromAssembly.InThisApplication(Assembly.GetEntryAssembly()));
 
-var foo = container.Resolve<IUsersService>();
-var result = await foo.Register("jane@doe.io", "Password1");
+var usersService = container.Resolve<IUsersService>();
+var result = await usersService.Register("jane@doe.io", "Password1");
 
 Console.ReadLine();
